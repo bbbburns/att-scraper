48bb4b50cf7e01453c75dcbb566059fcb50ef114
Jason Burns <burns.jason@gmail.com>
2024-01-07 10:47:22 -0500

    Add request time measurement
    Measure requests.elapsed and wall clock.
    Send as values to influxdb to monitor health.
    Add retry logic in unflux write.

--- get-att-bw.py
+++ get-att-bw.py
@@ -1,43 +1,25 @@
 """
 Pull the data in based on some stackoverflow beautifulsoup table examples
 
-Output the data to Influx 1.8 https://github.com/influxdata/influxdb-python
-
-Ideal JSON Data Structure for Measurement
-json_body = [
-{
-    "measurement": "net",
-    "tags": {
-        "host": "router",
-        "region": "livingstone"
-    },
-    "time": "2023-09-24T00:00:00Z",
-    "fields": {
-        "tx_bytes": X,
-        "tx_pkts": X,
-        "tx_err": X,
-        "tx_err_pct": X,
-        "rx_bytes": X,
-        "rx_pkts": X,
-        "rx_err": X,
-        "rx_err_pct": X
-    }
-}
-]
+Output the data to Influx 2 in line protocol
 
 Ideal line protocol for measurement. Line breaks added for clarity.
 
 net,host=router,region=livingstone tx_bytes=3713275163,tx_pkts=56434892,
                                    tx_err=0,tx_pct=0,rx_bytes=4909425,
-                                   rx_pkts109068990,rx_err=0,rx_pct=0
+                                   rx_pkts109068990,rx_err=0,rx_pct=0,
+                                   total_time=1234,elapsed_time=678
 """
 import toml
 import requests
+import time
+import sys
 from bs4 import BeautifulSoup
 import json
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -48,9 +30,9 @@ def append_fields(line_body, field_dict):
     Return the new line_body string.
 
     Take the list of fields from a dictionary and append them to the end
-    of the line_body string.
+    of the line_body string. Handle the commas correctly.
     """
-    
+
     for i, (key, value) in enumerate(field_dict.items()):
         if i + 1 == len(field_dict):
             # last item gets no comma at end
@@ -61,10 +43,11 @@ def append_fields(line_body, field_dict):
 
     return line_body
 
+
 def create_samples(values, type, sample_dict):
-    """ 
+    """
     Create samples takes in a single row of values and updates a shared dictionary.
-    
+
     Handle the case of tx and rx as separate types, but in the same dict structure.
     Operates on sample_dict{}
     """
@@ -141,20 +124,40 @@ def main():
     # print(f"Router IP: {router_ip} results in URL: {router_bw_url}")
 
     # Make the request
-    # TODO - add Retry
+    # Add Retry? No. Just fail. It can try again on the next collection.
+    start = time.time()
     response = requests.get(router_bw_url)
+
+    if not response.ok:
+        # If the router didn't respond OK we have nothing to log
+        # just exit to stop data from going to influx
+        # could consider just logging the response TIME instead of exiting
+        sys.exit(1)
+
+    # Wall clock time of the complete request and response with payload in milliseconds
+    total_time = int((time.time() - start) * 1000)
+
+    # Time to first byte according to Python requests, in milliseconds
+    elapsed_time = int(response.elapsed.total_seconds() * 1000)
 
     # Print the status code. Check this later
     # print(response)
+
+    # print(
+    #     f"Response.elapsed was {elapsed_time} milliseconds and total was {total_time} milliseconds"
+    # )
 
     # Process the response and update sample_dict{}
     sample_dict = {}
     parse_html(response, sample_dict)
 
+    # Add our response time measurements to the samples to estimate router / net health
+    sample_dict["total_time"] = total_time
+    sample_dict["elapsed_time"] = elapsed_time
+
     """
     Now we have a sample_dict{} that has everything we want. Have to pass this to influxdb
     Add measurement names and tags.
-    And a destination IP, pass(v 1.8, tokens if newer), and db name
     """
 
     # print("Sample Dictionary")
@@ -164,17 +167,21 @@ def main():
     # Simple measurement name - usually "net"
     measurement["measurement"] = influx_measurement
     # Simple tags for host and region. Could make this more flexible later if needed.
-    measurement["tags"] = { "host": router_host, "region": router_region }
-    # Dictionary of fields and their values
-    measurement["fields"] = sample_dict
+    measurement["tags"] = {"host": router_host, "region": router_region}
+    # Dictionary of fields and their values. Not used in my line protocol build approach
+    # measurement["fields"] = sample_dict
 
     # Now build first part of line protocol from the dictionary.
 
-    line_body = measurement["measurement"] + \
-                ",host=" + measurement["tags"]["host"] + \
-                ",region=" + measurement["tags"]["region"] + \
-                " "
-                
+    line_body = (
+        measurement["measurement"]
+        + ",host="
+        + measurement["tags"]["host"]
+        + ",region="
+        + measurement["tags"]["region"]
+        + " "
+    )
+
     # print("This is the first half of line format version.")
     # print(line_body)
 
@@ -189,12 +196,11 @@ def main():
     # Let's write line protocol instead.
     # client.write_points(line_body, protocol="line")
 
-    # setup urllib retries
-    #retries = urllib3.Retry(connect=3, read=2, redirect=3)
-    # how do I make that work with from_config_file?
+    # setup urllib retries to send to InfluxDB
+    retries = urllib3.Retry(connect=3, read=2, redirect=3)
 
     # 2.0 client from example
-    with InfluxDBClient.from_config_file("config.toml") as client:
+    with InfluxDBClient.from_config_file("config.toml", retries=retries) as client:
         with client.write_api(write_options=SYNCHRONOUS) as writer:
             writer.write(bucket=influx_bucket, record=line_body)
 
